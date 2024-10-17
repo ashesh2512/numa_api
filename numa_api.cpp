2eae643c5e5d9543814ea96743123b4b41091e25
Ashesh <ashesh.sharma@hpe.com>
2024-10-17 14:49:50 -0500

    add functions for getting the PID to CPU affinity

--- numa_api.cpp
+++ numa_api.cpp
@@ -1,13 +1,48 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <vector>
+
 #include <numa.h>
 
-#include <iostream>
-using namespace std;
- 
-// Main() function: where the execution of
-// program begins
-int main()
-{
-    std::cout << "Hello World" << std::endl;
- 
-    return 0;
+// Given PID, get the CPUs associated with it
+void get_cpu_affinity_to_pid(pid_t pid, std::vector<int>& cpu_list) {
+  // Allocate a CPU bitmask
+  struct bitmask *mask = numa_allocate_cpumask();
+  if (!mask) {
+    fprintf(stderr, "numa_allocate_cpumask failed");
+    return;
+  }
+
+  // Get the CPU affinity mask for the given PID
+  int result = numa_sched_getaffinity(pid, mask);
+  if (result < 0) {
+    fprintf(stderr, "numa_sched_getaffinity failed");
+    numa_free_cpumask(mask);
+    return;
+  }
+
+  // Gather the CPU associated with PID, and print affinity
+  printf("CPU affinity for PID %d: ", pid);
+  int num_possible_cpus = numa_num_possible_cpus();
+  for (int i = 0; i < num_possible_cpus; i++) {
+    if (numa_bitmask_isbitset(mask, i)) {
+      cpu_list.push_back(i);
+      printf("%d ", i);
+    }
+  }
+  printf("\n");
+
+  // Free the allocated mask
+  numa_free_cpumask(mask);
+}
+
+int main(int argc, char *argv[]) {
+
+  std::vector<int> cpu_list;
+  cpu_list.clear();
+
+  pid_t pid = getpid();
+  get_cpu_affinity_to_pid(pid, cpu_list);
+
+  return EXIT_SUCCESS;
 }
