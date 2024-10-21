f25193de4945b5320f5b2a0a27e6d56057e0c087
Ashesh <ashesh.sharma@hpe.com>
2024-10-21 17:17:17 -0500

    fix formatting

--- numa_api.cpp
+++ numa_api.cpp
@@ -4,15 +4,16 @@
 
 #include <numa.h>
 
-void isnuma_available()
-{
-  if (numa_available() == -1) {
-    fprintf(stderr, "numa library functions are undefined");
-  }
-}
-
 extern "C"
 {
+  void isnuma_available()
+  {
+    if (numa_available() == -1)
+    {
+      fprintf(stderr, "numa library functions are undefined");
+    }
+  }
+
   // Given PID, get the CPUs associated with it
   void get_cpu_affinity_to_pid(pid_t pid, int *cpu_list, int *cpu_count)
   {
@@ -62,42 +63,53 @@ extern "C"
 }
 
 // Get GPU affinity based on GPU for Frontier nodes
-int get_gpu_affinity_to_cpu_Frontier(int cpu) {
-  if ((cpu >= 0 && cpu <= 7) || (cpu >= 64 && cpu <= 71)) {
+int get_gpu_affinity_to_cpu_Frontier(int cpu)
+{
+  if ((cpu >= 0 && cpu <= 7) || (cpu >= 64 && cpu <= 71))
+  {
     return 4;
   }
-  else if ((cpu >= 8 && cpu <= 15) || (cpu >= 72 && cpu <= 79)) {
+  else if ((cpu >= 8 && cpu <= 15) || (cpu >= 72 && cpu <= 79))
+  {
     return 5;
   }
-  else if ((cpu >= 16 && cpu <= 23) || (cpu >= 80 && cpu <= 87)) {
+  else if ((cpu >= 16 && cpu <= 23) || (cpu >= 80 && cpu <= 87))
+  {
     return 2;
   }
-  else if ((cpu >= 24 && cpu <= 31) || (cpu >= 88 && cpu <= 95)) {
+  else if ((cpu >= 24 && cpu <= 31) || (cpu >= 88 && cpu <= 95))
+  {
     return 3;
   }
-  else if ((cpu >= 32 && cpu <= 39) || (cpu >= 96 && cpu <= 103)) {
+  else if ((cpu >= 32 && cpu <= 39) || (cpu >= 96 && cpu <= 103))
+  {
     return 6;
   }
-  else if ((cpu >= 40 && cpu <= 47) || (cpu >= 104 && cpu <= 111)) {
+  else if ((cpu >= 40 && cpu <= 47) || (cpu >= 104 && cpu <= 111))
+  {
     return 7;
   }
-  else if ((cpu >= 48 && cpu <= 55) || (cpu >= 112 && cpu <= 119)) {
+  else if ((cpu >= 48 && cpu <= 55) || (cpu >= 112 && cpu <= 119))
+  {
     return 0;
   }
-  else if ((cpu >= 56 && cpu <= 63) || (cpu >= 120 && cpu <= 127)) {
+  else if ((cpu >= 56 && cpu <= 63) || (cpu >= 120 && cpu <= 127))
+  {
     return 1;
   }
-  else {
+  else
+  {
     fprintf(stderr, "found cpu %d that shouldn't exist in Frontier architecture\n", cpu);
   }
 }
 
-int main(int argc, char *argv[]) {
-  //ensure numa library is reachable
+int main(int argc, char *argv[])
+{
+  // ensure numa library is reachable
   isnuma_available();
 
-  int* cpu_list = new int[128];
-  int* cpu_count = new int[1];
+  int *cpu_list = new int[128];
+  int *cpu_count = new int[1];
 
   pid_t pid = getpid();
   get_cpu_affinity_to_pid(pid, cpu_list, cpu_count);
