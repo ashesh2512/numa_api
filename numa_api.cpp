15dda40acbeb99705bb7e062e46d559ce1354a63
Ashesh Sharma <36968394+ashesh2512@users.noreply.github.com>
2025-12-09 15:46:38 -0700

    fix core/cpu terminology

--- numa_api.cpp
+++ numa_api.cpp
@@ -15,17 +15,17 @@ extern "C"
     }
   }
 
-  // Given PID, get the CPUs associated with it
-  void get_cpu_affinity_to_pid(pid_t pid, int* cpu_list, int* cpu_count)
+  // Given PID, get the cores associated with it
+  void get_core_affinity_to_pid(pid_t pid, int* core_list, int* core_count)
   {
-    std::vector<int> cpus;
+    std::vector<int> cores;
     // Allocate a CPU bitmask
     struct bitmask* cpu_mask = numa_allocate_cpumask();
     numa_bitmask_clearall(cpu_mask);
     if (!cpu_mask)
     {
       fprintf(stderr, "numa_allocate_cpumask failed\n");
-      *cpu_count = 0;
+      *core_count = 0;
       return;
     }
 
@@ -35,93 +35,93 @@ extern "C"
     {
       fprintf(stderr, "numa_sched_getaffinity failed\n");
       numa_free_cpumask(cpu_mask);
-      *cpu_count = 0;
+      *core_count = 0;
       return;
     }
 
-    // Gather the CPU associated with PID
-    printf("CPU affinity for PID %d: ", pid);
-    int num_possible_cpus = numa_num_possible_cpus();
-    for (int i = 0; i < num_possible_cpus; i++)
+    // Gather the core associated with PID
+    printf("core affinity for PID %d: ", pid);
+    int num_possible_cores = numa_num_possible_cpus();
+    for (int i = 0; i < num_possible_cores; i++)
     {
       if (numa_bitmask_isbitset(cpu_mask, i))
       {
-        cpus.push_back(i);
+        cores.push_back(i);
         printf("%d ", i);
       }
     }
     printf("\n");
 
     // Copy the results back to the output array
-    *cpu_count = cpus.size();
-    for (int i = 0; i < cpus.size(); i++)
+    *core_count = cores.size();
+    for (int i = 0; i < cores.size(); i++)
     {
-      cpu_list[i] = cpus[i];
+      core_list[i] = cores[i];
     }
 
     // Free the allocated mask
     numa_free_cpumask(cpu_mask);
   }
 
-  // Get GPU affinity based on CPU for Frontier nodes
-  int get_gpu_affinity_to_cpu_Frontier(int cpu)
+  // Get GPU affinity based on core for Frontier nodes
+  int get_gpu_affinity_to_core_Frontier(int core)
   {
-    if ((cpu >= 0 && cpu <= 7) || (cpu >= 64 && cpu <= 71))
+    if ((core >= 0 && core <= 7) || (core >= 64 && core <= 71))
     {
       return 4;
     }
-    else if ((cpu >= 8 && cpu <= 15) || (cpu >= 72 && cpu <= 79))
+    else if ((core >= 8 && core <= 15) || (core >= 72 && core <= 79))
     {
       return 5;
     }
-    else if ((cpu >= 16 && cpu <= 23) || (cpu >= 80 && cpu <= 87))
+    else if ((core >= 16 && core <= 23) || (core >= 80 && core <= 87))
     {
       return 2;
     }
-    else if ((cpu >= 24 && cpu <= 31) || (cpu >= 88 && cpu <= 95))
+    else if ((core >= 24 && core <= 31) || (core >= 88 && core <= 95))
     {
       return 3;
     }
-    else if ((cpu >= 32 && cpu <= 39) || (cpu >= 96 && cpu <= 103))
+    else if ((core >= 32 && core <= 39) || (core >= 96 && core <= 103))
     {
       return 6;
     }
-    else if ((cpu >= 40 && cpu <= 47) || (cpu >= 104 && cpu <= 111))
+    else if ((core >= 40 && core <= 47) || (core >= 104 && core <= 111))
     {
       return 7;
     }
-    else if ((cpu >= 48 && cpu <= 55) || (cpu >= 112 && cpu <= 119))
+    else if ((core >= 48 && core <= 55) || (core >= 112 && core <= 119))
     {
       return 0;
     }
-    else if ((cpu >= 56 && cpu <= 63) || (cpu >= 120 && cpu <= 127))
+    else if ((core >= 56 && core <= 63) || (core >= 120 && core <= 127))
     {
       return 1;
     }
     else
     {
-      fprintf(stderr, "found cpu %d that shouldn't exist in Frontier architecture\n", cpu);
+      fprintf(stderr, "found core %d that shouldn't exist in Frontier architecture\n", core);
     }
   }
 
   // Given PID, get the GPUs associated with it
   void get_gpu_affinity_to_pid_Frontier(pid_t pid, int* gpu_list, int* gpu_count) {
     
-    int* cpu_list = new int[128];
-    int* cpu_count = new int[1];
-    get_cpu_affinity_to_pid(pid, cpu_list, cpu_count);
+    int* core_list = new int[128];
+    int* core_count = new int[1];
+    get_core_affinity_to_pid(pid, core_list, core_count);
 
     std::set<int> unique_gpus; // will automatically store only unique values
 
-    for (int i = 0; i < *cpu_count; ++i) {
-      int cpu = cpu_list[i];
-      int gpu = get_gpu_affinity_to_cpu_Frontier(cpu);
+    for (int i = 0; i < *core_count; ++i) {
+      int core = core_list[i];
+      int gpu = get_gpu_affinity_to_core_Frontier(core);
       unique_gpus.insert(gpu);
     }
 
     // Sanity check for uniqueness
     if (unique_gpus.size() > 1) {
-      printf("Warning in PID %d: Not all CPUs are associated with the same GPU!\n", pid);
+      printf("Warning in PID %d: Not all cores are associated with the same GPU!\n", pid);
     } 
 
     // Populate the gpu_list with unique GPUs
@@ -135,8 +135,8 @@ extern "C"
     printf("\n");
 
     // Clean up dynamically allocated memory
-    delete[] cpu_list;
-    delete[] cpu_count;
+    delete[] core_list;
+    delete[] core_count;
   }
 }
 
